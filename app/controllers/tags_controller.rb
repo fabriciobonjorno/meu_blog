8ed791cdd0e91c58a0a32fa1c3bad944ec094695
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-02-12 23:32:31 -0400

    Config controller for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,45 @@
 class TagsController < ApplicationController
-  def index
+  before_action :set_tags, only: %i[edit update destory]
+
+  def new
+    @tag = Tag.new
   end
+
+  def create
+    @tag = Tag.new(tags_params)
+    if @tag.save
+      flash[:success] = 'Tag adicionada com sucesso'
+      redirect_to articles_path
+    else
+      flash[:error] = 'Erro ao adicionar tag'
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @tag.update(tags_params)
+      flash[:success] = 'Tag alterada com sucesso'
+      redirect_to articles_path
+    else
+      flash[:error] = 'Erro ao alterar tag'
+      render :edit
+    end
+  end
+
+  def destroy
+    if @tag.destroy
+      flash[:success] = 'Tag removida com sucesso'
+      redirect_to articles_path
+    else
+      flash[:error] = 'Erro ao remover tag'
+      redirect_to articles_path
+    end
+  end
+  
 
   private
 
@@ -10,7 +49,5 @@ class TagsController < ApplicationController
 
   def tags_params
     params.require(:tag).permit(:description)
-  end
-  
-  
+  end  
 end
